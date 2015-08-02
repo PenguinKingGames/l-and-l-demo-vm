f968e0faaa21ee1a6d3695c3c6cf1cfee7481226
Jye Nicolson <jyenicolson@gmail.com>
2015-08-02 23:37:21 +1000

    Candidate for not making the store a single gigantic ball of mud

---
+++ app/stores/StoreLibraries/lifeMap.js
@@ -0,0 +1,29 @@
+import * as screenTypes from '../../constants/Screens';
+
+export default {
+    preview: function(state, action) {
+      let currentMap = state.lifeMap;
+      let lifeMap = {
+        ...currentMap,
+        previewingScene: action.scene
+      };
+
+      return {
+        ...state,
+        lifeMap: lifeMap
+      };
+    },
+
+    select: function(state, action) {
+      let newScene = state.scenes[action.scene];
+      let selectedScene = {
+        ...newScene,
+        currentLine: newScene.initialLine
+      };
+      return {
+        ...state,
+        scene: selectedScene,
+        screen: screenTypes.SCENE
+      };
+    }
+}
